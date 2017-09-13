7fb9a9530f6330d11f19fdcfb59116948abb2022
Andy-from-fs <735188273@qq.com>
2017-09-13 10:43:06 +0800

    route config

--- src/lib/route.js
+++ src/lib/route.js
@@ -1,1 +1,66 @@
-import router from 'vue-router';
+import VueRouter from 'vue-router';
+
+const home = resolve => {
+  require.ensure(['../views/home/home.vue'], () => {
+    resolve(require('../views/home/home.vue'));
+  });
+}
+const welcome = resolve => {
+  require.ensure(['../views/home/welcome.vue'], () => {
+    resolve(require('../views/home/welcome.vue'));
+  });
+}
+const series = resolve => {
+  require.ensure(['../views/series/series.vue'], () => {
+    resolve(require('../views/series/series.vue'));
+  });
+}
+
+const routers = [{
+  path: '/',
+  name: 'index',
+  redirect: {
+    name: 'welcome'
+  },
+  children: [{
+    path: '/welcome',
+    name: 'welcome',
+    component: welcome
+  }, {
+    path: '/home',
+    name: 'home',
+    component: home,
+    meta:{
+      title:'奇新实木家具'
+    }
+  }, {
+    path: '/series-Europe',
+    name: 'series-Europe',
+    component: series,
+    meta:{
+      title:'欧风系列',
+      kind: 2,
+    }
+  }, {
+    path: '/series-huaxia',
+    name: 'series-huaxia',
+    component: 'series',
+    meta:{
+      title:'艺典华夏系列',
+      kind:0
+    }
+  }]
+}];
+
+const config={
+  mode:'history',
+  //  base: '/dist/',
+   base: '/cardTemplates/t3/',
+   routes: routers
+}
+const route =new VueRouter(config);
+route.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
+});
+export default route;
